b85f1cc7e61128b858b0dab07e821acdca596935
Karolis Narkevicius <karolis.n@gmail.com>
2019-08-08 14:22:33 +0100

    Not so fast!

---
+++ README.md
@@ -0,0 +1,125 @@
+# not-so-fast
+
+Fast and efficient in-memory rate-limit, used to alleviate most common DOS attacks.
+
+Based on https://github.com/valeriansaliou/node-fast-ratelimit, but updated to work with modern Node versions. This module uses the ES `Map` instead of the native `hashtable` package.
+
+
+## Usage
+
+```
+npm install --save not-so-fast
+```
+
+## How to use?
+
+The `not-so-fast` API is pretty simple, here are some keywords used in the docs:
+
+ * `ratelimiter`: ratelimiter instance, which plays the role of limits storage
+ * `namespace`: the master ratelimit storage namespace (eg: set `namespace` to the user client IP, or user username)
+
+You can create as many `ratelimiter` instances as you need in your application. This is great if you need to rate-limit IPs on specific zones (eg: for a chat application, you don't want the message send rate limit to affect the message composing notification rate limit).
+
+Here's how to proceed (we take the example of rate-limiting messages sending in a chat app):
+
+### 1. Create the rate-limiter
+
+The rate-limiter can be instanciated as such:
+
+```js
+const RateLimiter = require("not-so-fast");
+
+const messageLimiter = new RateLimiter({
+  threshold : 20, // available tokens over timespan
+  ttl       : 60  // time-to-live value of token bucket (in seconds)
+});
+```
+
+This limiter will allow 20 messages to be sent every minute per namespace.
+An user can send a maximum number of 20 messages in a 1 minute timespan, with a token counter reset every minute for a given namespace.
+
+The reset scheduling is done per-namespace; eg: if namespace `user_1` sends 1 message at 11:00:32am, he will have 19 messages remaining from 11:00:32am to 11:01:32am. Hence, his limiter will reset at 11:01:32am, and won't scheduler any more reset until he consumes another token.
+
+### 2. Check by consuming a token
+
+On the message send portion of our application code, we would add a call to the ratelimiter instance.
+
+#### 2.1. Consume token with asynchronous API (Promise catch/reject)
+
+```javascript
+// This would be dynamic in your application, based on user session data, or user IP
+namespace = "user_1";
+
+// Check if user is allowed to send message
+messageLimiter.consume(namespace)
+  .then(() => {
+    // Consumed a token
+    // Send message
+    message.send();
+  })
+  .catch(() => {
+    // No more token for namespace in current timespan
+    // Silently discard message
+  });
+```
+
+#### 2.2. Consume token with synchronous API (boolean test)
+
+```javascript
+// This would be dynamic in your application, based on user session data, or user IP
+namespace = "user_1";
+
+// Check if user is allowed to send message
+if (messageLimiter.consumeSync(namespace) === true) {
+  // Consumed a token
+  // Send message
+  message.send();
+} else {
+  // consumeSync returned false since there is no more tokens available
+  // Silently discard message
+}
+```
+
+### 3. Check without consuming a token
+
+In some instances, like password brute forcing prevention, you may want to check without consuming a token and consume only when password validation fails.
+
+#### 3.1. Check whether there are remaining tokens with asynchronous API (Promise catch/reject)
+
+```javascript
+limiter.hasToken(request.ip).then(() => {
+  return authenticate(request.login, request.password)
+})
+  .then(
+    () => {
+      // User is authenticated
+    },
+
+    () => {
+      // User is not authenticated
+      // Consume a token and reject promise
+      return limiter.consume(request.ip)
+        .then(() => Promise.reject())
+    }
+  )
+  .catch(() => {
+    // Either invalid authentication or too many invalid login
+    return response.unauthorized();
+  })
+```
+
+#### 3.2. Check whether there are remaining tokens with synchronous API (boolean test)
+
+```javascript
+if (!limiter.hasTokensSync(request.ip)) {
+  throw new Error("Too many invalid login");
+}
+
+const isAuthenticated = authenticateSync(request.login, request.password);
+
+if (!isAuthenticated) {
+  limiter.consumeSync(request.ip);
+
+  throw new Error("Invalid login/password");
+}
+```
---
+++ test/notSoFast.test.js
@@ -0,0 +1,345 @@
+/*
+ * node-fast-ratelimit
+ *
+ * Copyright 2019, Karolis Narkevicius
+ * Author: Karolis Narkevicius <hello@kn8.lt>
+ */
+
+const RateLimiter = require('../')
+const assert = require('assert')
+
+describe('node-fast-ratelimit', function() {
+  describe('constructor', function() {
+    it('should succeed creating a limiter with valid options', function() {
+      assert.doesNotThrow(
+        function() {
+          return new RateLimiter({
+            threshold: 5,
+            ttl: 10
+          })
+        },
+
+        'RateLimiter should not throw on valid options'
+      )
+    })
+
+    it('should fail creating a limiter with missing threshold', function() {
+      assert.throws(
+        function() {
+          return new RateLimiter({
+            ttl: 10
+          })
+        },
+
+        'RateLimiter should throw on missing threshold'
+      )
+    })
+
+    it('should fail creating a limiter with invalid threshold', function() {
+      assert.throws(
+        function() {
+          return new RateLimiter({
+            threshold: -1,
+            ttl: 10
+          })
+        },
+
+        'RateLimiter should throw on invalid threshold'
+      )
+    })
+
+    it('should fail creating a limiter with missing ttl', function() {
+      assert.throws(
+        function() {
+          return new RateLimiter({
+            threshold: 2
+          })
+        },
+
+        'RateLimiter should throw on missing ttl'
+      )
+    })
+
+    it('should fail creating a limiter with invalid ttl', function() {
+      assert.throws(
+        function() {
+          return new RateLimiter({
+            ttl: '120'
+          })
+        },
+
+        'RateLimiter should throw on invalid ttl'
+      )
+    })
+  })
+
+  describe('consumeSync method', function() {
+    it('should not rate limit an empty namespace', function() {
+      const limiter = new RateLimiter({
+        threshold: 100,
+        ttl: 10
+      })
+
+      assert.ok(limiter.consumeSync(null), 'Limiter consume should succeed for `null` (null) namespace (resolve)')
+
+      assert.ok(limiter.consumeSync(''), 'Limiter consume should succeed for `` (blank) namespace (resolve)')
+
+      assert.ok(limiter.consumeSync(0), 'Limiter consume should succeed for `0` (number) namespace (resolve)')
+    })
+
+    it('should not rate limit a single namespace', function() {
+      const options = {
+        threshold: 100,
+        ttl: 10
+      }
+
+      const namespace = '127.0.0.1'
+      const limiter = new RateLimiter(options)
+
+      for (let i = 1; i <= options.threshold; i++) {
+        assert.ok(limiter.consumeSync(namespace), 'Limiter consume should succeed')
+      }
+    })
+
+    it('should rate limit a single namespace', function() {
+      const namespace = '127.0.0.1'
+
+      const limiter = new RateLimiter({
+        threshold: 3,
+        ttl: 10
+      })
+
+      assert.ok(limiter.consumeSync(namespace), 'Limiter consume succeed at consume #1 (resolve)')
+
+      assert.ok(limiter.consumeSync(namespace), 'Limiter consume succeed at consume #2 (resolve)')
+
+      assert.ok(limiter.consumeSync(namespace), 'Limiter consume succeed at consume #3 (resolve)')
+
+      assert.ok(!limiter.consumeSync(namespace), 'Limiter consume fail at consume #4 (reject)')
+    })
+
+    it('should not rate limit multiple namespaces', function() {
+      const limiter = new RateLimiter({
+        threshold: 2,
+        ttl: 10
+      })
+
+      assert.ok(limiter.consumeSync('user_1'), 'Limiter consume should succeed at consume #1 of user_1 (resolve)')
+
+      assert.ok(limiter.consumeSync('user_2'), 'Limiter consume should succeed at consume #1 of user_2 (resolve)')
+    })
+
+    it('should rate limit multiple namespaces', function() {
+      const limiter = new RateLimiter({
+        threshold: 2,
+        ttl: 10
+      })
+
+      assert.ok(limiter.consumeSync('user_1'), 'Limiter consume should succeed at consume #1 of user_1 (resolve)')
+
+      assert.ok(limiter.consumeSync('user_2'), 'Limiter consume should succeed at consume #1 of user_2 (resolve)')
+
+      assert.ok(limiter.consumeSync('user_1'), 'Limiter consume should succeed at consume #2 of user_1 (resolve)')
+
+      assert.ok(limiter.consumeSync('user_2'), 'Limiter consume should succeed at consume #2 of user_2 (resolve)')
+
+      assert.ok(!limiter.consumeSync('user_1'), 'Limiter consume should fail at consume #3 of user_1 (reject)')
+
+      assert.ok(!limiter.consumeSync('user_2'), 'Limiter consume should fail at consume #3 of user_2 (reject)')
+    })
+
+    it('should expire token according to TTL', function(done) {
+      // Do not consider timeout as slow
+      this.slow(5000)
+
+      const options = {
+        threshold: 2,
+        ttl: 1
+      }
+
+      const namespace = '127.0.0.1'
+      const limiter = new RateLimiter(options)
+
+      assert.ok(limiter.consumeSync(namespace), 'Limiter consume should succeed at consume #1 (resolve)')
+
+      assert.ok(limiter.consumeSync(namespace), 'Limiter consume should succeed at consume #2 (resolve)')
+
+      assert.ok(!limiter.consumeSync(namespace), 'Limiter consume should fail at consume #3 (reject)')
+
+      // Wait for TTL reset.
+      setTimeout(function() {
+        assert.ok(limiter.consumeSync(namespace), 'Limiter consume should succeed at consume #4 (resolve)')
+
+        done()
+      }, options.ttl * 1000 + 100)
+    })
+
+    it('should not block writing random namespaces', function(done) {
+      // Timeout if longer than 2 seconds (check for blocking writes)
+      this.timeout(2000)
+
+      const limiter = new RateLimiter({
+        threshold: 100,
+        ttl: 60
+      })
+
+      const asyncFlowSteps = 10000
+      const asyncFlowTotal = 4
+      let asyncFlowCountDone = 0
+
+      const launchAsyncFlow = function(id) {
+        setTimeout(function() {
+          for (let i = 0; i < asyncFlowSteps; i++) {
+            assert.ok(
+              limiter.consumeSync('flow-' + id + '-' + i),
+              'Limiter consume should succeed at flow #' + id + ' (resolve)'
+            )
+          }
+
+          if (++asyncFlowCountDone === asyncFlowTotal) {
+            done()
+          }
+        })
+      }
+
+      // Launch asynchronous flows
+      for (let i = 1; i <= asyncFlowTotal; i++) {
+        launchAsyncFlow(i)
+      }
+    })
+  })
+
+  describe('hasTokenSync method', function() {
+    it('should not consume token', function() {
+      const limiter = new RateLimiter({
+        threshold: 1,
+        ttl: 10
+      })
+      const namespace = '127.0.0.1'
+
+      assert.ok(limiter.hasTokenSync(namespace), 'Limiter hasTokenSync should succeed at hasTokenSync #1')
+      assert.ok(limiter.hasTokenSync(namespace), 'Limiter hasTokenSync should succeed at hasTokenSync #2')
+    })
+
+    it('should rate limit', function() {
+      const limiter = new RateLimiter({
+        threshold: 1,
+        ttl: 10
+      })
+      const namespace = '127.0.0.1'
+
+      assert.ok(limiter.hasTokenSync(namespace), 'Limiter hasTokenSync should succeed at hasTokenSync #1')
+      assert.ok(limiter.consumeSync(namespace), 'Limiter consumeSync should succeed at consumeSync #1')
+      assert.ok(!limiter.hasTokenSync(namespace), 'Limiter hasTokenSync should fail at hasTokenSync #2')
+    })
+  })
+
+  describe('hasToken method', function() {
+    it('should not consume token', function(done) {
+      const limiter = new RateLimiter({
+        threshold: 1,
+        ttl: 10
+      })
+      const namespace = '127.0.0.1'
+      const allPromises = []
+
+      allPromises.push(limiter.hasToken(namespace))
+      allPromises.push(limiter.hasToken(namespace))
+
+      Promise.all(allPromises)
+        .then(function() {
+          done()
+        })
+        .catch(function(error) {
+          if (error.message !== 'No tokens available') {
+            done(error)
+          } else {
+            done(new Error('Limiter hasToken should not fail at the end (reject)'))
+          }
+        })
+    })
+
+    it('should rate limit', function(done) {
+      const limiter = new RateLimiter({
+        threshold: 1,
+        ttl: 10
+      })
+      const namespace = '127.0.0.1'
+      const allPromises = []
+
+      allPromises.push(limiter.hasToken(namespace))
+      allPromises.push(limiter.consume(namespace))
+      allPromises.push(limiter.hasToken(namespace))
+
+      Promise.all(allPromises)
+        .then(function() {
+          done(new Error('Limiter hasToken should not succeed at the end (reject)'))
+        })
+        .catch(function(error) {
+          if (error.message !== 'No tokens available') {
+            done(error)
+          } else {
+            done()
+          }
+        })
+    })
+  })
+
+  describe('consume method', function() {
+    it('should not rate limit', function(done) {
+      const options = {
+        threshold: 100,
+        ttl: 10
+      }
+
+      const namespace = '127.0.0.1'
+      const limiter = new RateLimiter(options)
+
+      const allPromises = []
+
+      for (let i = 1; i <= options.threshold; i++) {
+        allPromises.push(limiter.consume(namespace))
+      }
+
+      Promise.all(allPromises)
+        .then(function() {
+          done()
+        })
+        .catch(function(error) {
+          if (error.message !== 'No tokens available') {
+            done(error)
+          } else {
+            done(new Error('Limiter consume should not fail at the end (reject)'))
+          }
+        })
+    })
+
+    it('should rate limit', function(done) {
+      const options = {
+        threshold: 100,
+        ttl: 10
+      }
+
+      const namespace = '127.0.0.1'
+      const limiter = new RateLimiter(options)
+
+      const allPromises = []
+
+      for (let i = 1; i <= options.threshold + 5; i++) {
+        allPromises.push(limiter.consume(namespace))
+      }
+
+      Promise.all(allPromises)
+        .then(function() {
+          done(new Error('Limiter consume should not succeed at the end (reject)'))
+        })
+        .catch(function(error) {
+          if (error.message !== 'No tokens available') {
+            done(error)
+          } else {
+            done()
+          }
+        })
+    })
+  })
+})
